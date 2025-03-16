1b2e3c32d9e1f390b268b734e4855b93a9f14abe
Roger <rogermateom@gmail.com>
2025-03-16 19:21:38 +0100

    update

---
+++ AEA3/Alquileres_MATEO/Alquiler.java
@@ -0,0 +1,68 @@
+public class Alquiler {
+    private String nombre;
+    private int capacidad;
+    private boolean disp;
+
+    Alquiler(String nombre, int capacidad){
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.disp = true;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getCapacidad() {
+        return capacidad;
+    }
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+    public boolean getDisp() {
+        return disp;
+    }
+    public void setDisp(boolean disp) {
+        this.disp = disp;
+    }
+
+    public double costeNoche(int numPersones) {
+        return 0;
+    }
+
+    public void mostrarInformacio() {
+        System.out.println(this.getNombre());
+        System.out.println("Capacidad: " + this.getCapacidad());
+        System.out.print("Disponible: ");
+        if (this.getDisp()){
+            System.out.println("Sí");
+        } else{
+            System.out.println("No");
+        }
+        System.out.println("Coste de noche: " + costeNoche(capacidad));
+    }
+
+    public void reservar() {
+        if (this.disp){
+            this.disp = false;
+            System.out.println("Reservado correctamente");
+        }
+        else 
+            System.out.println("Ya esta reservada");
+    }
+
+    public void liberar() {
+        if (!this.disp){
+            this.disp = true;
+            System.out.println("Liberado correctamente");
+        }
+        else 
+            System.out.println("No esta reservada");
+    }
+
+    public boolean disponible() {
+        return disp;
+    }
+}
---
+++ AEA3/Alquileres_MATEO/Habitacion.java
@@ -0,0 +1,28 @@
+
+public class Habitacion extends Alquiler {
+    private int numCamas;
+    public Habitacion(String nombre, int capacidad, int numCamas){
+        super(nombre, capacidad);
+        this.setNumCamas(numCamas);
+    }
+    
+    public int getNumCamas() {
+        return numCamas;
+    }
+    public void setNumCamas(int numCamas) {
+        this.numCamas = numCamas;
+    }
+    @Override
+    public double costeNoche(int numPersones) {
+        if (numCamas < 3)
+            return 50;
+        else
+            return 50 + (numCamas -3) * 20;
+    }
+
+    @Override
+    public void mostrarInformacio() {
+        super.mostrarInformacio();
+        System.out.println("Numero de camas: " + this.getNumCamas());
+    }
+}
