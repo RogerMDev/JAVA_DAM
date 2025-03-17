fa612faa5232b94e796750c0508aeda9a7d779cd
Roger <rogermateom@gmail.com>
2025-03-17 22:30:15 +0100

    New files

---
+++ AEA3/ALQUILERES_MATEO/CasaRural.java
@@ -0,0 +1,51 @@
+public class CasaRural extends Alquiler {
+    private boolean piscina;
+    private boolean jardin;
+
+    CasaRural(String nombre, int capacidad, boolean piscina, boolean jardin){
+        super(nombre, capacidad);
+        this.setPiscina(piscina);
+        this.setJardin(jardin);
+    }
+
+    public boolean getPiscina() {
+        return piscina;
+    }
+
+    public void setPiscina(boolean piscina) {
+        this.piscina = piscina;
+    }
+
+    public boolean getJardin() {
+        return jardin;
+    }
+
+    public void setJardin(boolean jardin) {
+        this.jardin = jardin;
+    }
+
+    @Override
+    public double costeNoche(int numPersones) {
+        if (piscina)
+            return 200;
+        else
+            return 150;
+    }
+    
+    @Override
+    public void mostrarInformacio() {
+        super.mostrarInformacio();
+        System.out.print("Piscina: ");
+        if (this.getPiscina()){
+            System.out.println(" Sí");
+        } else{
+            System.out.println(" No");
+        }
+        System.out.print("Jardin: ");
+        if ( this.getJardin()){
+            System.out.println(" Sí");
+        } else {
+            System.out.println(" No");
+        }
+    }
+}
