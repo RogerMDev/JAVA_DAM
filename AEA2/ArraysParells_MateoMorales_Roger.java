d3c11d87348156a5ff074a496ab50812cbfe38be
Roger <rogermateom@gmail.com>
2025-05-05 19:41:39 +0200

    - ArrayIndexOutOfBoundsException prevented

--- AEA2/ArraysParells_MateoMorales_Roger.java
+++ AEA2/ArraysParells_MateoMorales_Roger.java
@@ -1,19 +1,26 @@
 
 public class ArraysParells_MateoMorales_Roger{
     public static void main ( String[] args){
+    try {
+        int nombres[] = new int[100];
 
-    //Creem l'Array amb 100 'posicions'
-    int nombres[] = new int[100];
-
-    nombres[0] = 0;
+        nombres[0] = 0;
 
         for (int i = 0; i< nombres.length; i++){
             nombres[i] = i*2;
         }
         System.out.println("La llista dels primers 100 nombres parells es:  ");
-        for (int i = 0; i< nombres.length; i++){
-            System.out.print(nombres[i] +", ");
+        for (int i = 0; i<= nombres.length; i++){
+
+            System.out.print(nombres[i] + " ");
         }
+        
+    } catch (ArrayIndexOutOfBoundsException indexOut) {
+        System.out.println("Hi ha hagut un error: " + indexOut);
+    } catch(Exception e){
+        System.out.println("Error " + e);
+    }
     }
 }
+
 
