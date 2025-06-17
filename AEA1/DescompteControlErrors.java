883c706d1d0c02bccbdf251996bdfc7cca72e0b3
Roger Mateo <rogermateom@gmail.com>
2025-06-17 19:21:12 +0200

    test commit

--- AEA1/DescompteControlErrors.java
+++ AEA1/DescompteControlErrors.java
@@ -1,14 +1,13 @@
-import java.util.Scanner;
 
 public class DescompteControlErrors {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
         int comptador = 0;
 
         System.out.println("El comptador val 0");
         while (comptador < 10) {
         System.out.println("Comptador +1");
+        comptador +=1;
         }
         System.out.println("El comptador ha arribat a 10");
     }
