55eca42955d1ebad92eac28d0d384fd0c51280c8
Roger Mateo <rogermateom@gmail.com>
2025-09-29 20:11:52 +0200

    PracticaPOO

--- PROVES_JAVA_ROGER/Proves/EstructuresDeDades/Array/main.java
+++ PROVES_JAVA_ROGER/Proves/EstructuresDeDades/Array/main.java
@@ -1,10 +1,9 @@
 package array;
 
-public class main{
+public class Main{
 
     public static void main(String[] args) {
         double[] notas = {5,4,1,8.7,4,4.6,7.2,3.1,0.4};
         System.out.println(EstadisticasClase.analizarNotas(notas));
-
     }
 }
